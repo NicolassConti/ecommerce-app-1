9f25ca9cdf49eb55526bc2aec726563a4e16df58
nicolassconti <continicolas6@gmail.com>
2023-02-03 17:34:47 -0300

    first commit

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -3,6 +3,7 @@ import { Container, Nav, Navbar, NavDropdown, Offcanvas } from "react-bootstrap"
 import { Link, useNavigate } from "react-router-dom";
 import PurchasesSidebar from "./PurchasesSidebar"
 
+
 const AppNavbar = () => {
 
   const navigate = useNavigate();
@@ -19,16 +20,16 @@ const AppNavbar = () => {
   return (
     <>
       <Navbar expand="lg" variant="dark" bg="primary">
-        <Container>
-          <Navbar.Brand as={Link} to="/" style={{ fontSize: 25, }}>E-commerce</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/" style={{ fontSize: 25, marginLeft: 20 }}>E-commerce</Navbar.Brand>
+        <Container style={{marginLeft: 1150 }}>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/login" style={{ fontSize: 20 }} ><i className='bx bxs-user'></i></Nav.Link>
+              <Nav.Link as={Link} to="/login" style={{ fontSize: 20 }} >Login <i className='bx bxs-user'></i></Nav.Link>
               <Nav.Link as={Link} to="/product/id" style={{ fontSize: 20 }}>Product Detail</Nav.Link>
               <Nav.Link as={Link} to="/purchases" style={{ fontSize: 20 }}>Purchases</Nav.Link>
               <Nav.Link onClick={logout} style={{ fontSize: 20 }}>Log out <i class='bx bx-log-out'></i></Nav.Link>
-              <Nav.Link onClick={handleShow} style={{ fontSize: 20 }}><i className='bx bxs-cart'></i></Nav.Link>
+              <Nav.Link onClick={handleShow} style={{ fontSize: 20 }}>Cart<i className='bx bxs-cart'></i></Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
